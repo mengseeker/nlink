63c1a45378b2ff79fa4fd5aa0ad1d783cddf3f25
xingbiao <xingbiao@sensetime.com>
2023-12-20 14:46:01 +0800

    update

--- scripts/update-bin.bash
+++ scripts/update-bin.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-OS=("windows" "darwin" "linux")
+OS=("linux" "windows" "darwin")
 ARCH=("amd64" "arm64")
 
 # 使用for循环遍历数组
@@ -11,6 +11,11 @@ for GOOS in "${OS[@]}"; do
         out=build/bin/$binName
         CGO_ENABLED='0' GOOS=$GOOS GOARCH=$GOARCH go build -o $out cmd/main/main.go
         curl -X PUT -T $out http://hugohome.codenative.net:9000/public/nlink/$binName
-        echo http://hugohome.codenative.net:9000/public/nlink/$binName
+        
+        binName=nlink-gui-$GOOS-$GOARCH
+        if [[ "$GOOS" == windows ]]; then binName=$binName.exe; fi
+        out=build/bin/$binName
+        CGO_ENABLED='1' GOOS=$GOOS GOARCH=$GOARCH wails build -debug -noPackage -o nlink-gui-$GOOS-$GOARCH
+        curl -X PUT -T $out http://hugohome.codenative.net:9000/public/nlink/$binName
     done
 done
